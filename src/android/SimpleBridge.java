1e0f85c333411ab6ac0c3015c15b16e16b21ee6f
jrxjsj <juan.sanchez@jaraxa.com>
2018-04-27 17:32:13 +0200

    Added a way to call from native code to JS and get response in callbacks

--- src/android/SimpleBridge.java
+++ src/android/SimpleBridge.java
@@ -7,18 +7,32 @@ import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.PluginResult;
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
 
 import android.util.Log;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class SimpleBridge extends CordovaPlugin {
 
 	private static final String TAG = "SimpleBridge";
+	private static Map<CordovaWebView, SimpleBridge> sInstances = new HashMap<CordovaWebView, SimpleBridge>();
+
+	private CallbackContext mNativeToJsCallbackContext;
+	private CordovaWebView mWebView;
+	private AtomicInteger mTid;
+	private Map<Integer,JsCallback> mSuccessCallbacks;
+    private Map<Integer,JsCallback> mErrorCallbacks;
 
     /**
      * Constructor.
      */
     public SimpleBridge() {
-		//TODO
+        mTid = new AtomicInteger(0);
+        mSuccessCallbacks = new HashMap<Integer, JsCallback>();
+        mErrorCallbacks = new HashMap<Integer, JsCallback>();
     }
 
     /**
@@ -31,7 +45,8 @@ public class SimpleBridge extends CordovaPlugin {
     @SuppressWarnings("deprecation")
     public void initialize(CordovaInterface cordova, CordovaWebView webView) {
         super.initialize(cordova, webView);
-		//TODO
+        mWebView = webView;
+        sInstances.put(mWebView, this);
     }
 
     /**
@@ -39,16 +54,30 @@ public class SimpleBridge extends CordovaPlugin {
      *
      * @param action                The action to execute.
      * @param args          	    JSONArry of arguments for the plugin.
-     * @param callbackS=Context     The callback id used when calling back into JavaScript.
+     * @param callbackContext       The callback id used when calling back into JavaScript.
      * @return              	    True if the action was valid.
      * @throws JSONException 
      */
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-		Log.e(TAG, action + " args: " + args);
-        if (action.equals("executeNative")) {
-            //TODO
-			PluginResult dataResult = new PluginResult(PluginResult.Status.OK);
-			callbackContext.sendPluginResult(dataResult);			
+		Log.d(TAG, action + " args: " + args);
+        if (action.equals("init")) {
+            PluginResult dataResult = new PluginResult(PluginResult.Status.OK, new JSONArray());
+            dataResult.setKeepCallback(true);
+            mNativeToJsCallbackContext = callbackContext;
+            mNativeToJsCallbackContext.sendPluginResult(dataResult);
+        } else if (action.equals("executeNative")) {
+            PluginResult dataResult = new PluginResult(PluginResult.Status.OK, new JSONArray());
+			callbackContext.sendPluginResult(dataResult);
+        } else if (action.equals("callSuccess")) {
+            Integer tid = args.getInt(0);
+            args.remove(0);
+            runSuccessCallback(tid, args);
+            callbackContext.success();
+        } else if (action.equals("callError")) {
+            Integer tid = args.getInt(0);
+            args.remove(0);
+            runErrorCallback(tid, args);
+            callbackContext.success();
         } else {
             // Unsupported action
             return false;
@@ -56,5 +85,60 @@ public class SimpleBridge extends CordovaPlugin {
         return true;
     }
 
+    private void runSuccessCallback(Integer tid, JSONArray args) {
+        JsCallback callback = mSuccessCallbacks.get(tid);
+        clearCallbacks(tid);
+        if (callback != null) {
+            callback.done(args);
+        }
+    }
+
+    private void runErrorCallback(Integer tid, JSONArray args) {
+        JsCallback callback = mSuccessCallbacks.get(tid);
+        clearCallbacks(tid);
+        if (callback != null) {
+            callback.done(args);
+        }
+    }
+
+    private void clearCallbacks(Integer tid) {
+        mSuccessCallbacks.remove(tid);
+        mErrorCallbacks.remove(tid);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        sInstances.remove(mWebView);
+    }
+
+    public Integer callJS(String method, JSONArray args, JsCallback successCallback, JsCallback errorCallback) throws JSONException {
+        Integer tid = mTid.addAndGet(1);
+        Log.d(TAG, method + " tid: " + tid + " args: " + args);
+        if (successCallback != null) {
+            mSuccessCallbacks.put(tid, successCallback);
+        }
+        if (errorCallback != null) {
+            mErrorCallbacks.put(tid, errorCallback);
+        }
+        JSONArray allArgs = new JSONArray();
+        allArgs.put(tid);
+        allArgs.put(method);
+        for (int i = 0; i < args.length(); i++) {
+            allArgs.put(args.get(i));
+        }
+        PluginResult dataResult = new PluginResult(PluginResult.Status.OK, allArgs);
+        dataResult.setKeepCallback(true);
+        mNativeToJsCallbackContext.sendPluginResult(dataResult);
+        return tid;
+    }
+
+    public static SimpleBridge getInstance(CordovaWebView webView) {
+        return sInstances.get(webView);
+    }
+
+    public static abstract class JsCallback {
+        public abstract void done(JSONArray params);
+    }
 }
 
--- www/simplebridge.js
+++ www/simplebridge.js
@@ -1,10 +1,47 @@
-var exec = require("cordova/exec");
+var exec = require("cordova/exec"),
+    channel = require('cordova/channel');
 
-var simplebridge = {
-	executeNative: function(successCallback, errorCallback, method, params) {
-	    var newParams = params.slice();
-	    newParams.unshift(method);		
-		exec(successCallback, errorCallback, "SimpleBridge", "executeNative", newParams);
-	}
-};
+var simplebridge = (function () {
+    var _exportedApi;
+
+    channel.onCordovaReady.subscribe(function() {
+        var successCallback = function(params) {
+            console.log("SimpleBridge from native ", params);
+            if (params.length >= 2) {
+                var tid = params[0];
+                var method = params[1];
+                var restParams = params.slice(2);
+                if (typeof(_exportedApi[method]) !== "undefined") {
+                    _exportedApi[method](function(params){
+                        var newParams = params.slice();
+                        newParams.unshift(tid);
+                        exec(null, null, 'SimpleBridge', 'callSuccess', newParams);
+                    },function(error){
+                        var newParams = params.slice();
+                        newParams.unshift(tid);
+                        exec(null, null, 'SimpleBridge', 'callError', newParams);
+                    }, restParams);
+                }
+            } else {
+                console.log("SimpleBridge init");
+            }
+        };
+        var errorCallback = function(params) {
+            console.log("SimpleBridge from native (ERROR) ", params);
+        };
+        exec(successCallback, errorCallback, 'SimpleBridge', 'init', []);
+    });
+
+    return {
+        executeNative: function(successCallback, errorCallback, method, params) {
+            var newParams = params.slice();
+            newParams.unshift(method);
+            exec(successCallback, errorCallback, "SimpleBridge", "executeNative", newParams);
+        },
+        exportApi: function(api) {
+            _exportedApi = api;
+        }
+    };
+})();
+
 module.exports = simplebridge;
